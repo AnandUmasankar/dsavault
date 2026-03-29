850a03e9057aaa9b148c9f4c39839f59ab22bae2
AnandUmasankar <anandanumashankar@gmail.com>
2026-03-29 19:01:11 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import rehypeMermaid from 'rehype-mermaid';
-import rehypeMermaidPre from './src/plugins/remark-mermaid-pre.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dsavault.in',
   integrations: [mdx(), sitemap()],
-  markdown: {
-    rehypePlugins: [rehypeMermaidPre, [rehypeMermaid, { strategy: 'inline-svg' }]],
-  },
   vite: {
     plugins: [tailwindcss()],
   },
